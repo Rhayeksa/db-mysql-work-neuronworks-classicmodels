d950a106dd186babc8cd1f068a57ec82add7d35b
rhayeksa_neuron <rhayeksads@gmail.com>
2023-05-06 04:23:37 +0700

    complete module 1 sorting_data

---
+++ scripts/module_1/sorting_data.sql
@@ -0,0 +1,17 @@
+SELECT
+    employeeNumber,
+    firstName,
+    lastName,
+    hire_date,
+    salary
+FROM classicmodels.employees
+ORDER BY firstName;
+
+SELECT
+    employeeNumber,
+    firstName,
+    lastName,
+    hire_date,
+    salary
+FROM classicmodels.employees
+ORDER BY salary DESC;
